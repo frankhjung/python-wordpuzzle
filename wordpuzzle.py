f674cf8716b08bc21d864265e16d9c03c5892864
Frank Jung <frankhjung@linux.com>
2021-04-10 17:51:26 +1000

    update email

--- wordpuzzle.py
+++ wordpuzzle.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
         prog=os.path.basename(sys.argv[0]),
         usage='%(prog)s [options]',
         description='Solve 9 letter word puzzle.',
-        epilog='© 2019-2021 Frank Jung mailto:frank.jung@marlo.com.au')
+        epilog='© 2019-2021 Frank Jung mailto:frankhjung@linux.com')
     PARSER.add_argument(
         '-d',
         '--dictionary',
